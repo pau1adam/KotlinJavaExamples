2be0417a6d35b4e4893d05ead92c27fdd0231c2b
Pavlos Adam <pavlosadam@gmail.com>
2018-06-06 15:00:50 +0300

    Added retrofit call to RecipeActivity.

--- app/src/main/java/io/github/pau1adam/kotlinjavasamples/java/RecipeActivity.java
+++ app/src/main/java/io/github/pau1adam/kotlinjavasamples/java/RecipeActivity.java
@@ -1,7 +1,7 @@
 package io.github.pau1adam.kotlinjavasamples.java;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.widget.ArrayAdapter;
 import android.widget.ImageView;
 import android.widget.ListView;
@@ -10,24 +10,44 @@ import android.widget.TextView;
 import com.squareup.picasso.Picasso;
 
 import io.github.pau1adam.kotlinjavasamples.R;
+import io.github.pau1adam.kotlinjavasamples.kotlin.GetRecipeResponse;
 import io.github.pau1adam.kotlinjavasamples.kotlin.Recipe;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.moshi.MoshiConverterFactory;
 
 public class RecipeActivity extends AppCompatActivity {
 
-    ImageView imageView;
-    TextView textView;
-    ListView listView;
+    private ImageView imageView;
+    private TextView textView;
+    private ListView listView;
+
+    private final String BASE_URL = "http://food2fork.com/api/";
+
+    private final String API_KEY = "add api key";
+
+    Retrofit retrofit = new Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .addConverterFactory(MoshiConverterFactory.create())
+            .build();
+
+    Food2ForkApi api;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(final Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_recipe);
         imageView = findViewById(R.id.imageView);
         textView = findViewById(R.id.textview);
         listView = findViewById(R.id.ingerdients_listview);
 
-        Recipe recipe = new Recipe();
-        loadUI(recipe);
+        api = retrofit.create(Food2ForkApi.class);
+
+        Call<GetRecipeResponse> searchRecipeCall = api.getRecipe(API_KEY, "potato");
+        searchRecipeCall.enqueue(responseCallback);
+
     }
 
     private void loadUI(Recipe recipe) {
@@ -43,4 +63,20 @@ public class RecipeActivity extends AppCompatActivity {
         );
         listView.setAdapter(adapter);
     }
+
+    private final Callback<GetRecipeResponse> responseCallback = new Callback<GetRecipeResponse>() {
+        @Override
+        public void onResponse(Call<GetRecipeResponse> call, Response<GetRecipeResponse> response) {
+            if (response.body() != null) {
+                final Recipe recipe = response.body().getRecipe();
+                loadUI(recipe);
+            }
+        }
+
+        @Override
+        public void onFailure(Call<GetRecipeResponse> call, Throwable t) {
+            final Recipe recipe = new Recipe();
+            loadUI(recipe);
+        }
+    };
 }
